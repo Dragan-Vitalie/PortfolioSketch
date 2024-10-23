4274eb329cd5118b00c4d8bdc1075b39493d6188
Vitalie Dragan <vitalie.dragan@songkick.com>
2024-10-23 14:42:28 +0100

    change experience info & design

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -3,24 +3,98 @@ import React from 'react';
 const Experience = () => {
   return (
     <section className="section-content">
-      <h2 className="text-4xl font-bold mb-6">Work Experience</h2>
+      <h2 className="text-4xl font-bold mb-2">Software Engineer at Warner Music Group  </h2>
+      <h3 className="text-gray-400 text-xl mb-6">Apprenticeship | June 2023 - Present</h3>
       <div className="mb-8">
-        <h3 className="text-xl font-semibold">Software Engineer Apprenticeship</h3>
-        <p className="text-gray-400 mb-2">Warner Music Group | June 2023 - Present</p>
-        <ul className="list-disc list-inside text-gray-300">
-          <li>Main developer for a key feature that reduces event editing time by over 90%</li>
-          <li>Led page modernization and spam link checking project</li>
-          <li>Co-developed feature for testing code changes in specific environments</li>
-          <li>Contributed to Auth proof of concept and Ruby upgrade across services</li>
-        </ul>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center">Event Management</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>Main developer for a key feature</strong> that reduces the time required for managers of artists to edit existing events by over 90%, from 25 minutes to under 2 minutes.</p>
+            <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center ">Page Modernization</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>Main developer for page modernization</strong>, from old server side ruby code to a fancy looking client side rendering. Responsible for key documentation <strong>PRD</strong> (Product Requirement Document), <strong>EDD</strong> (Engineering Design Document)</p>
+            <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">API</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center ">Trust and Safety</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>Implemented spam link checking </strong> using Google's WebRisk API as part of this work enhancing trust and safety with Google's WebRisk API.</p>
+            <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">API</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center ">Improve Testing</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>Co-developed a feature </strong> enabling different types of users with distinct permissions to test new code changes in a specific environment before deployment to production. This enhancement reduced the time spent testing features impacting different user roles by over 93%.</p>
+            <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">API</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center ">POC using Auth0</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>Contributed to a proof of concept using Auth0 </strong>, including Figma wireframes and React integrations, which was subsequently integrated into the Website, Dashboard, and App.</p>
+            <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">API</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div>
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center ">Ruby Upgrade</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>Contributed to the Ruby upgrade </strong>, across multiple services, verifying gems and tests before pipeline deployment, while following best practices and reporting a critical outdated gem issue.</p>
+            <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">API</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div>
+          </div>
+        </div>
+        <div className="bg-gray-800 mt-7 p-6 justify-center rounded-lg">
+          <h3 className="text-xl font-semibold mb-2 flex justify-between items-center "> Following Best Practices within B2B</h3>
+          <p className="text-gray-300 mb-4"><strong className='text-lime-300'>Working within the B2B team </strong>, adopting Agile methodology while prioritizing CI/CD, continuous learning, and team productivity.</p>
+          <div className="flex gap-2">
+            <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+            <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">API</span>
+            <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+          </div>
+        </div>
       </div>
-      <div>
-        <h3 className="text-xl font-semibold">Construction Supervisor</h3>
-        <p className="text-gray-400 mb-2">Kendell | September 2022 - June 2023</p>
-        <ul className="list-disc list-inside text-gray-300">
-          <li>Supervised a team of laborers and skilled workers</li>
-          <li>Collaborated with manager to optimize work efficiency and save company money</li>
-        </ul>
+
+
+      <h2 className="text-4xl font-bold mt-20 mb-2">Construction Supervisor</h2>
+      <h3 className="text-gray-400 text-xl mb-6">Kendell | September 2022 - June 2023</h3>
+      <div className="mb-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center">Management</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>-</strong> Supervised a team of laborers and skilled workers</p>
+            {/* <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div> */}
+          </div>
+          <div className="bg-gray-800 p-6 rounded-lg">
+            <h3 className="text-xl font-semibold mb-2 flex justify-between items-center ">Efficiency</h3>
+            <p className="text-gray-300 mb-4"><strong className='text-lime-300'>-</strong> Collaborated with manager to optimize work efficiency and save company money</p>
+            {/* <div className="flex gap-2">
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">React</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">API</span>
+              <span className="bg-gray-700 text-gray-300 px-2 py-1 rounded text-sm">Ruby on Rails</span>
+            </div> */}
+          </div>
+        </div>
       </div>
     </section>
   );
